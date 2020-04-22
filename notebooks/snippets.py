6f7ef8cb6dc5bf4c53b11af800c6b481f3570738
Blaž Zupančič <43244418+blazzup@users.noreply.github.com>
2020-04-22 21:44:29 +0200

    Add code snippets for random functionality

---
+++ notebooks/snippets.py
@@ -0,0 +1,63 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ### Use SVG format for displaying plots inside notebook (better image quality)
+
+# %%
+# %config InlineBackend.figure_format = 'svg'
+
+# %% [markdown]
+# ### Switch to interactive mode (opens separate window)
+
+# %%
+# %matplotlib qt
+
+# %% [markdown]
+# ### Switch to inline mode (closes the interactive figure!)
+
+# %%
+# %matplotlib inline
+
+# %% [markdown]
+# ### Generate test data frames
+
+# %%
+import pandas as pd
+import pandas.util.testing as tu
+
+# %%
+pd.np.random.seed(0)
+tu.N = 3
+tu.K = 5
+tu.makeDataFrame()
+
+# %% [markdown]
+# ### Show all rows or columns
+
+# %%
+pd.options.display.expand_frame_repr = False
+
+# the following lines are needed if dataframe actually contains more rows or columns
+pd.options.display.max_rows = 0
+pd.options.display.max_columns = 0
+
+# %% [markdown]
+# ### Restart current kernel without dialog
+
+# %%
+from IPython.core.display import HTML
+
+HTML("<script>Jupyter.notebook.kernel.restart()</script>")
